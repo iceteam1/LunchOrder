1cef89963255824925427e31a295a53ab53d1076
EB-SchoolAccount <144454887+EB-SchoolAccount@users.noreply.github.com>
2023-09-11 17:48:03 -0400

    Updated Main.java
    I replaced the default code with the experimental code that can read the txt file. I made it a branch in case we still need the original code.

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.io.*;
 
 
 class Main {
   
  
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException{
     System.out.println("LunchOrder lab -  Intro to OOP");
     
     
@@ -18,23 +19,26 @@ class Main {
 		Scanner input =  new Scanner(System.in);
 		DecimalFormat format = new DecimalFormat("#.00");
 		
-		
-		
-	Food burger = new Food(1.85, 9, 33, 1);	
-	
-	Food salad = new Food(2.00, 1, 11, 5);
-	
+    //taken from https://stackoverflow.com/questions/30564462/read-data-from-a-text-file-and-create-an-object
+    Scanner fileInput = new Scanner(new File("items.txt"));
+    fileInput.useDelimiter("-|\n");
+    ArrayList<Food> menuList = new ArrayList<Food>();
 
-	System.out.println("Enter number of burgers: ");
-	int burgers = input.nextInt();
-	System.out.println(burger.toString("burger"));
-	total = total + (burger.getPrice() * burgers);
-	
-	
-	
-	System.out.println(salad.toString("salad"));
+    while(fileInput.hasNext()){
+      String food = fileInput.next();
+      double price =  Double.valueOf(fileInput.next().substring(1));;
+      int fat = fileInput.nextInt();
+      int Carbs = fileInput.nextInt();
+      int Fiber = fileInput.nextInt();
 
-	
+      menuList.add(new Food(food, price, fat, Carbs, Fiber));
+    }
+
+    for(int i = 0; i < menuList.size(); i++){
+      System.out.println(menuList.get(i));
+    }
+
+    
 
 	System.out.println("Your total is: $ " + format.format(total));
 	
